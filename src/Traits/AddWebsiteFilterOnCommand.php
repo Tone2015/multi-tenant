37db723ae2200e0de313a157bf8333028800119c
王涛 <wangtao@kuainiugroup.com>
2019-05-14 16:00:48 +0800

    修复tenancy:db:seed  命令  指定租户website_id时，执行的代码还是在system 数据库中执行的BUG

--- src/Traits/AddWebsiteFilterOnCommand.php
+++ src/Traits/AddWebsiteFilterOnCommand.php
@@ -14,6 +14,7 @@
 
 namespace Hyn\Tenancy\Traits;
 
+use Hyn\Tenancy\Environment;
 use Illuminate\Database\Eloquent\Collection;
 use Symfony\Component\Console\Input\InputOption;
 
@@ -27,9 +28,13 @@ trait AddWebsiteFilterOnCommand
             $query->whereIn('id', (array) $this->option('website_id'));
         }
 
-        $query->orderBy('id')->chunk(10, function (Collection $websites) use ($callable) {
-            $websites->each(function ($website) use ($callable, $websites) {
+        //获取租户环境
+        $environment = app(Environment::class);
+        $query->orderBy('id')->chunk(10, function (Collection $websites) use ($callable, $environment) {
+            $websites->each(function ($website) use ($callable, $websites, $environment) {
                 $this->connection->set($website);
+                //设置租户
+                $environment->tenant($website);
 
                 is_callable($callable) ? $callable($website) : parent::handle();
 
